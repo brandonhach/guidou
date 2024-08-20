176b2afd79ee03c34c0fef6d322b71b93debd2ff
brandonhach <specialcambo@gmail.com>
2024-08-20 10:28:45 -0400

    onclick functions callback to auth service

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import {
   ionLogoApple,
@@ -10,6 +10,7 @@ import {
   ionLogIn,
 } from '@ng-icons/ionicons';
 import { diGoogleOriginal } from '@ng-icons/devicon/original';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -27,4 +28,22 @@ import { diGoogleOriginal } from '@ng-icons/devicon/original';
     }),
   ],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  userData = signal({});
+  constructor(private auth: AuthService, private router: Router) {
+    this.auth.currentUser.subscribe((user) => {
+      console.log(user);
+      this.userData.set(user?.user_metadata?.['email']);
+    });
+  }
+
+  async handleAuth() {
+    const response = await this.auth.signInWithGithub();
+    console.log(response);
+  }
+
+  async signOut() {
+    this.auth.signOut();
+    this.router.navigate(['/']);
+  }
+}
