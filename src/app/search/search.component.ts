fd9479d73490ed55fcb5b4f6327d6de87f4d023e
brandonhach <specialcambo@gmail.com>
2024-08-22 15:34:09 -0400

    implement forecast box and control flow to child component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,19 +2,26 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from '../services/db.service';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { AccuService } from '../services/accu.service';
+import { ForecastBoxComponent } from '../shared/forecast-box/forecast-box.component';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, ForecastBoxComponent],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
 export class SearchComponent implements OnInit {
   public guides: any[] = [];
   public query: string | undefined;
+  public forecastData: any;
 
-  constructor(public route: ActivatedRoute, private db: DatabaseService) {}
+  constructor(
+    public route: ActivatedRoute,
+    private db: DatabaseService,
+    private accu: AccuService
+  ) {}
 
   async ngOnInit() {
     this.route.params.subscribe(async (params) => {
@@ -22,6 +29,11 @@ export class SearchComponent implements OnInit {
       if (query !== '') {
         this.guides = await this.db.fetchGuideByCity(query);
         this.query = query;
+        console.log('query', query);
+        this.accu.getHourlyForecast(query).subscribe((data) => {
+          console.log('Forecast', data);
+          this.forecastData = data;
+        });
       }
     });
   }
