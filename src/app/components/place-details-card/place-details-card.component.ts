a4cea9b32fb746fefd72ca2b97f37faefa778041
brandonhach <specialcambo@gmail.com>
2024-08-26 11:01:20 -0400

    completed migrating place details card from guidou-v2

--- src/app/components/place-details-card/place-details-card.component.ts
+++ src/app/components/place-details-card/place-details-card.component.ts
@@ -1,12 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PlaceSearchResult } from '../place-autocomplete/place-autocomplete.component';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { ionPin } from '@ng-icons/ionicons';
 
 @Component({
   selector: 'app-place-details-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgIconComponent, CommonModule, NgOptimizedImage],
   templateUrl: './place-details-card.component.html',
+  viewProviders: [
+    provideIcons({
+      ionPin,
+    }),
+  ],
 })
 export class PlaceDetailsCardComponent implements OnInit {
   @Input() data: PlaceSearchResult | undefined;
