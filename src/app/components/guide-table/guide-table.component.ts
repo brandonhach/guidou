1330ddff7d1e603c60f4d71bd8bdd7535be5f59d
brandonhach <specialcambo@gmail.com>
2024-08-26 15:00:28 -0400

    finished table to display fetch guides

--- src/app/components/guide-table/guide-table.component.ts
+++ src/app/components/guide-table/guide-table.component.ts
@@ -1,27 +1,44 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { DatabaseService } from '../../services/db.service';
 import { CommonModule } from '@angular/common';
 import { PlaceSearchResult } from '../place-autocomplete/place-autocomplete.component';
+
+type Guide = {
+  id: string;
+  city: string;
+  description: string;
+  created_at: string;
+  user_id: string;
+  title: string;
+};
 
 @Component({
   selector: 'app-guide-table',
   standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './guide-table.component.html',
-  styles: ``,
 })
-export class GuideTableComponent {
-  public guides: any[] = [];
+export class GuideTableComponent implements OnChanges {
+  public guides: Guide[] = [];
+  public isLoading: boolean = false;
   @Input() to: PlaceSearchResult | undefined;
 
   constructor(private db: DatabaseService) {}
 
-  async ngOnInit() {
-    if (this.to) {
-      await this.db.fetchGuideByCity(this.to.address).then((guides) => {
-        this.guides = guides;
-      });
+  async ngOnChanges(changes: SimpleChanges) {
+    if (changes['to'] && this.to) {
+      this.isLoading = true;
+      try {
+        const address = this.to.address;
+        console.log('address:', address);
+        this.guides = await this.db.fetchGuideByCity(this.to.address);
+        console.log('Guides updated:', this.guides); // Debugging line
+      } catch (error) {
+        console.error('Error fetching guides:', error);
+      } finally {
+        this.isLoading = false;
+      }
     }
   }
 }
