2a0f808fcb0d9e7491ed5fdfd2fa74849b76ca55
brandonhach <specialcambo@gmail.com>
2024-08-26 11:34:07 -0400

    added forecast display to template

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -8,6 +8,7 @@ import {
 } from '../components/place-autocomplete/place-autocomplete.component';
 import { PlaceDetailsCardComponent } from '../components/place-details-card/place-details-card.component';
 import { MapDisplayComponent } from '../components/map-display/map-display.component';
+import { ForecastDisplayComponent } from '../components/forecast-display/forecast-display.component';
 
 @Component({
   selector: 'app-explore',
@@ -19,6 +20,7 @@ import { MapDisplayComponent } from '../components/map-display/map-display.compo
     PlaceAutocompleteComponent,
     PlaceDetailsCardComponent,
     MapDisplayComponent,
+    ForecastDisplayComponent,
   ],
   templateUrl: './explore.component.html',
 })
